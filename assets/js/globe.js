ed6451ce9a7bfeeed007fabde8d2b9e29bb9b473
Sergei I'MON <jabba.person@gmail.com>
2026-08-20 22:15:22 +0300

    fix: version final Figma assets for Pages

--- assets/js/globe.js
+++ assets/js/globe.js
@@ -7,7 +7,7 @@ import {
   prepareLandPoints,
   projectPoint,
   wrapPi
-} from './globe-model.js';
+} from './globe-model.js?v=figma-12-2-pixel-final';
 
 const canvas = document.getElementById('axe-globe');
 const globeShell = document.querySelector('.hero__globe-shell');
